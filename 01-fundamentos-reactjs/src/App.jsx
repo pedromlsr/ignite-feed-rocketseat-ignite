1b8a1c627f2d383d3b0d3599aa79dd5829f485d5
pedromlsr <pedrodesarodrigues@gmail.com>
2023-07-04 23:03:49 -0300

    project scaffold + introduction + css modules

---
+++ 01-fundamentos-reactjs/src/App.jsx
@@ -0,0 +1,20 @@
+import { Post } from "./Post"
+import { Header } from "./components/Header"
+
+import './styles.css'
+
+export function App() {
+  return (
+    <div>
+      <Header />
+      <Post
+        author="Pedro Rodrigues"
+        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam perspiciatis cumque suscipit iste, blanditiis, consectetur ea fugit id obcaecati, quidem quasi eligendi tempore reprehenderit quos commodi veniam sapiente explicabo voluptatem!"
+      />
+      <Post
+        author="Outro cara"
+        content="Outra assunto"
+      />
+    </div>
+  )
+}
