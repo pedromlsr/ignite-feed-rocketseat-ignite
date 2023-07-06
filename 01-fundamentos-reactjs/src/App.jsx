0f1b788f8ab7befbedc7fe6b35f796c1a8bfe7ce
pedromlsr <pedrodesarodrigues@gmail.com>
2023-07-05 23:42:41 -0300

    component sidebar

--- 01-fundamentos-reactjs/src/App.jsx
+++ 01-fundamentos-reactjs/src/App.jsx
@@ -1,20 +1,29 @@
-import { Post } from "./Post"
-import { Header } from "./components/Header"
+import { Post } from "./Post";
+import { Header } from "./components/Header";
+import { Sidebar } from "./components/Sidebar";
 
-import './global.css'
+import styles from './App.module.css';
+import './global.css';
 
 export function App() {
   return (
     <div>
       <Header />
-      <Post
-        author="Pedro Rodrigues"
-        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam perspiciatis cumque suscipit iste, blanditiis, consectetur ea fugit id obcaecati, quidem quasi eligendi tempore reprehenderit quos commodi veniam sapiente explicabo voluptatem!"
-      />
-      <Post
-        author="Outro cara"
-        content="Outra assunto"
-      />
+      <div className={styles.wrapper}>
+        <aside>
+          <Sidebar />
+        </aside>
+        <main>
+          <Post
+            author="Pedro Rodrigues"
+            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam perspiciatis cumque suscipit iste, blanditiis, consectetur ea fugit id obcaecati, quidem quasi eligendi tempore reprehenderit quos commodi veniam sapiente explicabo voluptatem!"
+          />
+          <Post
+            author="Outro cara"
+            content="Outra assunto"
+          />
+        </main>
+      </div>
     </div>
   )
 }
