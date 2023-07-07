a3008563c3650cb0cd289b53d3b8fd5c469884da
pedromlsr <pedrodesarodrigues@gmail.com>
2023-07-07 00:20:51 -0300

    component comment

---
+++ 01-fundamentos-reactjs/src/components/Comment.jsx
@@ -0,0 +1,35 @@
+import { ThumbsUp, Trash } from 'phosphor-react'
+
+import styles from './Comment.module.css'
+
+export function Comment() {
+  return (
+    <div className={styles.comment}>
+      <img className={styles.avatar} src="https://github.com/pedromlsr.png" />
+
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>Pedro Rodrigues</strong>
+              <time title="07 de Julho às 11:10" dateTime="2023-07-06 11:10:00">Cerca de 1h atrás</time>
+            </div>
+
+            <button title="Deletar comentário">
+              <Trash size={20} />
+            </button>
+          </header>
+          
+          <p>Muito bom Devon, parabéns!! 👏👏</p>
+        </div>
+
+        <footer>
+          <button>
+            <ThumbsUp />
+            Aplaudir <span>20</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  )
+}
--- 01-fundamentos-reactjs/src/components/Post.jsx
+++ 01-fundamentos-reactjs/src/components/Post.jsx
@@ -1,3 +1,4 @@
+import { Comment } from './Comment'
 import styles from './Post.module.css'
 
 export function Post() {
@@ -22,16 +23,25 @@ export function Post() {
         <p>
           <a href="">#novoprojeto</a>{' '}
           <a href="">#nlw</a>{' '}
-          <a href="">#rocketseat</a></p>
+          <a href="">#rocketseat</a>
+        </p>
       </div>
 
       <form className={styles.commentForm}>
         <strong>Deixe seu feedback</strong>
+        
         <textarea placeholder="Deixe um comentário" />
+        
         <footer>
           <button type="submit">Publicar</button>
         </footer>
       </form>
+
+      <div className={styles.commentList}>
+        <Comment />
+        <Comment />
+        <Comment />
+      </div>
     </article>
   )
 }
