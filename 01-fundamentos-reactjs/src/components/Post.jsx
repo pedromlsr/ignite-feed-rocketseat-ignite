5b3bd0419daed95842f9dbd98e1f73b5e034b1a7
pedromlsr <pedrodesarodrigues@gmail.com>
2023-07-08 00:00:13 -0300

    avatar component

--- 01-fundamentos-reactjs/src/components/Post.jsx
+++ 01-fundamentos-reactjs/src/components/Post.jsx
@@ -1,3 +1,4 @@
+import { Avatar } from './Avatar'
 import { Comment } from './Comment'
 import styles from './Post.module.css'
 
@@ -6,7 +7,7 @@ export function Post() {
     <article className={styles.post}>
       <header>
         <div className={styles.author}>
-          <img className={styles.avatar} src="https://github.com/pedromlsr.png" />
+          <Avatar src="https://github.com/Talles-Souza.png" />
           <div className={styles.authorInfo}>
             <strong>Pedro Rodrigues</strong>
             <span>Web Developer</span>
--- 01-fundamentos-reactjs/src/components/Sidebar.jsx
+++ 01-fundamentos-reactjs/src/components/Sidebar.jsx
@@ -1,4 +1,5 @@
 import { PencilLine } from 'phosphor-react'
+import { Avatar } from './Avatar';
 
 import styles from './Sidebar.module.css';
 
@@ -12,7 +13,7 @@ export function Sidebar() {
       />
       
       <div className={styles.profile}>
-        <img className={styles.avatar} src="http://github.com/pedromlsr.png" />
+        <Avatar src="https://github.com/pedromlsr.png" />
         <strong>Pedro Rodrigues</strong>
         <span>Web Developer</span>
       </div>
