b555a68b4f9e1dcf887f5d4f758c8d064f909450
pedromlsr <pedrodesarodrigues@gmail.com>
2023-07-12 23:51:51 -0300

    communication between components

--- 01-fundamentos-reactjs/src/components/Comment.jsx
+++ 01-fundamentos-reactjs/src/components/Comment.jsx
@@ -3,7 +3,11 @@ import { Avatar } from './Avatar'
 
 import styles from './Comment.module.css'
 
-export function Comment({ content }) {
+export function Comment({ content, onDeleteComment }) {
+  function handleDeleteComment() {
+    onDeleteComment(content)
+  }
+  
   return (
     <div className={styles.comment}>
       <Avatar hasBorder={false} src="https://github.com/pedromlsr.png" />
@@ -16,7 +20,7 @@ export function Comment({ content }) {
               <time title="07 de Julho às 11:10" dateTime="2023-07-06 11:10:00">Cerca de 1h atrás</time>
             </div>
 
-            <button title="Deletar comentário">
+            <button onClick={handleDeleteComment} title="Deletar comentário">
               <Trash size={24} />
             </button>
           </header>
