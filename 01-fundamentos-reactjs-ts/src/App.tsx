e631ddd549ae3701bbc2a1e24f9763ff29ed8afc
pedromlsr <pedrodesarodrigues@gmail.com>
2023-07-14 00:38:27 -0300

    adapting project to typescript

---
+++ 01-fundamentos-reactjs-ts/src/App.tsx
@@ -0,0 +1,67 @@
+import { Post } from "./components/Post";
+import { Header } from "./components/Header";
+import { Sidebar } from "./components/Sidebar";
+
+import styles from './App.module.css';
+
+import './global.css';
+
+// author: { avatarUrl: "", name: "", role: "" }
+// publishedAt: Date
+// content: String
+
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https://github.com/pedromlsr.png',
+      name: 'Pedro Rodrigues',
+      role: 'Trainee Programmer'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋' },
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀' },
+      { type: 'link', content: 'jane.design/doctorcare' },
+    ],
+    publishedAt: new Date('2023-07-08 11:21:00')
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: 'https://github.com/Talles-Souza.png',
+      name: 'Talles Souza',
+      role: 'Trainee Programmer'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋' },
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀' },
+      { type: 'link', content: 'jane.design/doctorcare' },
+    ],
+    publishedAt: new Date('2023-07-08 11:21:00')
+  },
+]
+
+export function App() {
+  return (
+    <div>
+      <Header />
+      <div className={styles.wrapper}>
+        <aside>
+          <Sidebar />
+        </aside>
+        <main>
+          {posts.map(post => {
+            return (
+              <Post
+                key={post.id}
+                author={post.author}
+                content={post.content}
+                publishedAt={post.publishedAt}
+              />
+            )
+          })}
+        </main>
+      </div>
+    </div>
+  )
+}
---
+++ 01-fundamentos-reactjs-ts/src/components/Sidebar.tsx
@@ -0,0 +1,29 @@
+import { PencilLine } from 'phosphor-react'
+import { Avatar } from './Avatar';
+
+import styles from './Sidebar.module.css';
+
+export function Sidebar() {
+  return (
+    <aside className={styles.sidebar}>
+      <img
+        className={styles.cover}
+        src="https://images.unsplash.com/photo-1500964757637-c85e8a162699?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=50"
+        alt=""
+      />
+      
+      <div className={styles.profile}>
+        <Avatar src="https://github.com/pedromlsr.png" />
+        <strong>Pedro Rodrigues</strong>
+        <span>Web Developer</span>
+      </div>
+
+      <footer>
+        <a href="#">
+          <PencilLine size={20} />
+          Editar seu perfil
+        </a>
+      </footer>
+    </aside>
+  )
+}
